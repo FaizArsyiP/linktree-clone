09d77dd9d05bec0978869d1aecdf248e931b2438
FaizArsyiP <faizarsyipragata@mail.ugm.ac.id>
2024-03-30 09:59:41 +0700

    tes

---
+++ main.js
@@ -0,0 +1,43 @@
+// console.log("Hello World");
+
+// let nama = "Faiz Arsyi P";
+// nama = "Tes";
+
+// console.log(nama)
+
+// const angka = "123";
+// console.log(typeof angka)
+
+// const PI = 3.14
+// const r = 10
+// const luas = PI*r**2
+// console.log(luas)
+
+
+// for( i=0;i<=3;i++){
+//     console.log(i);
+// }
+
+// const nama = "Faiz";
+// const umur = 20;
+// const jurusan ="Teknologi Informasi"
+// const perkenalan = `Hai,
+// aku ${nama}. Aku berumur ${umur}. Aku berasal dari jurusan ${jurusan}`
+// console.log(perkenalan)
+// const perkenalan = (nama, umur, jurusan) =>{
+//     return `hai, aku ${nama}. umurku ${umur} aku dri jurusan ${jurusan}.`
+// }
+// console.log(perkenalan("Faiz","18","Teknologi Informasi"))
+// console.log(perkenalan("orang","10","Teknologi Informasi"))
+
+// let angka = [2,3,4,5];
+// //mapping
+// const hasil = angka.map((angkas)=>angkas**2)
+// console.log(hasil)
+
+// //filter
+// const ganjil = angka.filter((filt)=>filt%2==1)
+// console.log(ganjil)
+
+// alert("Halo")
+// prompt("tes")
